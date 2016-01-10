f593db2104bcf37249d115f8a69ebc509cc6e7da
SomeAnnoyingGuy <ThisIsNotBenOK@gmail.com>
2016-01-09 22:23:12 -0500

    another test

--- src/cinderthorne/CINDERTHORNE.java
+++ src/cinderthorne/CINDERTHORNE.java
@@ -1,6 +1,8 @@
 package cinderthorne;
 
 public class CINDERTHORNE {
+	//Test 2.0
+	//hello worrrllllddddd
 	public static void main(String[] args){
 		System.out.println("This is a testerino");
 	}
