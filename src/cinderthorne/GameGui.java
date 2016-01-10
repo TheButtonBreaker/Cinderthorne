eff021df92c80e86d76f37eca8dc74d62cf13b0b
SomeAnnoyingGuy <ThisIsNotBenOK@gmail.com>
2016-01-10 00:07:51 -0500

    Formatting

--- src/cinderthorne/GameGui.java
+++ src/cinderthorne/GameGui.java
@@ -6,26 +6,15 @@ import java.awt.event.MouseEvent;
 
 public class GameGui {
 	public void draw(Graphics2D g, int width, int height){
-		
 	}
-
 	public void mouseDown(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 	}
-	
 	public void mouseUp(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 	}
-	
 	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 	}
-
 	public void keyDown(KeyEvent arg0) {
-		// TODO Auto-generated method stub
 	}
-	
 	public void keyUp(KeyEvent arg0) {
-		// TODO Auto-generated method stub
 	}
 }
