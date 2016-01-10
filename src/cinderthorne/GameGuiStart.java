27b49b3fccd42ce5be547ecd71ba5a5e1c59fb14
bennyman123abc <bennyman123abc@hotmail.com>
2016-01-09 23:40:19 -0500

    Revert "Rendering GameGuiStart modularity fix"
    This reverts commit 29cc683ebbab20d4d27f0871a449d0fe1f5b3970.

--- src/cinderthorne/GameGuiStart.java
+++ src/cinderthorne/GameGuiStart.java
@@ -1,47 +1,5 @@
 package cinderthorne;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.Graphics2D;
-import java.awt.GraphicsEnvironment;
-import java.io.File;
-import java.io.IOException;
-
 public class GameGuiStart extends GameGui {
-	public GameGuiStart(){
-		createFont();
-	}
-	
-	static double rot; //Meant to make the title rotate back and forth subtly like in Minecraft
-	@Override
-	public void draw(Graphics2D g, int width, int height){
-		final Dimension RESOLUTION = Rendering.RESOLUTION;
 
-		g.setFont(createCinderFont(0,100));
-		g.setColor(Color.white);
-		String str = "Cinderthorne";
-		int strwidth = g.getFontMetrics().stringWidth(str);
-		g.rotate(rot, width/2+50, height/2+50);
-		g.drawString(str, RESOLUTION.width/2-strwidth/2, g.getFontMetrics().getHeight()+20);
-		g.drawRect(width/2, height/2, 100, 100);
-	}
-	
-	private static Font cinderFont = null;
-
-	public static Font createCinderFont(int style, float size){
-		return cinderFont.deriveFont(style).deriveFont(size);
-	}
-	
-	public static void createFont() {
-		try {
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			Font font = Font.createFont(Font.TRUETYPE_FONT, new File("Resources/Cinderthorne Font.ttf"));
-			ge.registerFont(font);
-			cinderFont = font;
-		} catch (IOException | FontFormatException e) {
-			e.printStackTrace();
-		}
-	}
 }
