5936a976c43d9c00b950e4d3546048ae75f5ca4b
SomeAnnoyingGuy <ThisIsNotBenOK@gmail.com>
2016-01-10 13:41:05 -0500

    Tiny fix

--- src/cinderthorne/Entity.java
+++ src/cinderthorne/Entity.java
@@ -10,38 +10,43 @@ public class Entity {
 	public int locY;
 	private int size;
 	public int movespeed = 3;
-	
-	public Entity(int size){
+
+	public Entity(int size) {
 		this.size = size;
 	}
-	
-	public Rectangle getHitbox(){
-		return new Rectangle(locX,locY,size,size);
+
+	public Rectangle getHitbox() {
+		return new Rectangle(locX, locY, size, size);
 	}
-	
-	public Rectangle getHitbox(int x, int y){
-		return new Rectangle(x,y,size,size);
+
+	public Rectangle getHitbox(int x, int y) {
+		return new Rectangle(x, y, size, size);
 	}
-	
-	public boolean canMoveTo(Point targetPoint, TileMap map){
+
+	public boolean canMoveTo(Point targetPoint, TileMap map) {
 		Rectangle hitbox = getHitbox(targetPoint.x, targetPoint.y);
-		if(hitbox.x < 0 || hitbox.x+hitbox.getWidth() > map.getTiles().length*Tile.SIZE){
+		
+		if (hitbox.x < 0 || hitbox.x + hitbox.getWidth() > map.getTiles().length * Tile.SIZE) {
 			return false;
 		}
+		if (hitbox.y < 0 || hitbox.y + hitbox.getHeight() > map.getTiles()[0].length * Tile.SIZE) {
+			return false;
+		}
+
 		return true;
 	}
-	
-	public void attemptMove(int right, int down, TileMap map){
+
+	public void attemptMove(int right, int down, TileMap map) {
 		down *= movespeed;
 		right *= movespeed;
-		if(canMoveTo(new Point(locX + right, locY + down),map)){
+		if (canMoveTo(new Point(locX + right, locY + down), map)) {
 			locX += right;
 			locY += down;
 		}
 	}
-	
-	public void draw(Graphics2D g, int xOff, int yOff){
+
+	public void draw(Graphics2D g, int xOff, int yOff) {
 		g.setColor(Color.red);
-		g.drawRect(xOff+locX, yOff+locY, size, size);
+		g.drawRect(xOff + locX, yOff + locY, size, size);
 	}
 }
