62b4e518d00a2407df8bef67f3f7d6c12842dbbb
bennyman123abc <bennyman123abc@hotmail.com>
2016-01-09 23:28:00 -0500

    Revert "Rendering GameGuiStart modularity fix"
    This reverts commit 29cc683ebbab20d4d27f0871a449d0fe1f5b3970.

--- src/cinderthorne/GameGui.java
+++ src/cinderthorne/GameGui.java
@@ -2,16 +2,8 @@ package cinderthorne;
 
 import java.awt.Graphics2D;
 
-/**
- * 
- * @author Taiga
- * These guys are basically a gui in the gui.
- * hard to explain
- * whatever
- * just put up with it
- */
 public class GameGui {
-	public void draw(Graphics2D g, int width, int height){
+	public void draw(Graphics2D g){
 		
 	}
 }
