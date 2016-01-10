29cc683ebbab20d4d27f0871a449d0fe1f5b3970
SomeAnnoyingGuy <ThisIsNotBenOK@gmail.com>
2016-01-09 23:04:41 -0500

    Rendering GameGuiStart modularity fix

--- src/cinderthorne/GameGui.java
+++ src/cinderthorne/GameGui.java
@@ -2,8 +2,16 @@ package cinderthorne;
 
 import java.awt.Graphics2D;
 
+/**
+ * 
+ * @author Taiga
+ * These guys are basically a gui in the gui.
+ * hard to explain
+ * whatever
+ * just put up with it
+ */
 public class GameGui {
-	public void draw(Graphics2D g){
+	public void draw(Graphics2D g, int width, int height){
 		
 	}
 }
